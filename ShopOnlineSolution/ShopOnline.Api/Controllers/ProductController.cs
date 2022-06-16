708d0f431a79aa41fe2d84436de3c64013fe6cd6
Glaucos Ginez <glaucosginez@gmail.com>
2022-06-16 21:40:20 +0100

    Products controller

---
+++ ShopOnlineSolution/ShopOnline.Api/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopOnline.Api.Extensions;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository productsRepository;
+
+        public ProductController(IProductRepository productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProductsAsync()
+        {
+            var products = await this.productsRepository.GetAllProductsAsync();
+            var categories = await this.productsRepository.GetAllProductCategoriesAsync();
+            var productsDto = products.ConvertToDto(categories);
+
+            return Ok(productsDto);
+        }
+    }
+}
---
+++ ShopOnlineSolution/ShopOnline.Api/Extensions/DtoExtensions.cs
@@ -0,0 +1,30 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Models;
+
+namespace ShopOnline.Api.Extensions
+{
+    public static class DtoExtensions
+    {
+        public static IEnumerable<ProductDto> ConvertToDto(
+            this IEnumerable<Product> products,
+            IEnumerable<ProductCategory> categories)
+        {
+            return products.Join(
+                categories,
+                p => p.CategoryId,
+                c => c.Id,
+                (p, c) => new ProductDto
+                    {
+                        Id = p.Id,
+                        Name = p.Name,
+                        CategoryId = p.CategoryId,
+                        CategoryName = c.Name,
+                        Price = p.Price,
+                        Description = p.Description,
+                        ImageURL = p.ImageURL,
+                        Qty = p.Qty,
+                    }
+                );
+        }
+    }
+}
